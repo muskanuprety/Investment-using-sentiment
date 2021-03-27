ea85a5c75c4d5cd06260e0daa17059479a14f48c
Muskan Uprety <muskan.uprety@outlook.com>
2021-03-27 11:41:23 -0400

    final update script

--- process.sh
+++ process.sh
@@ -1,7 +1,4 @@
 # process so far
-pip install requests_html bs4 psaw psycopg2 pandas tweepy alpha_vantage csv sqlalchemy praw
-
-
 
 # you can use create-database.py to create a table. But the code in extract_stock_info.py takes care of creating a table if it doesn't exist.
 python extract_stock_info.py # for now it will download data for TSLA, MRNA, GME, PFE, AAPL, PTON. You can change the tickers in the main() of the python file
@@ -10,24 +7,29 @@ python extract_stock_info.py # for now it will download data for TSLA, MRNA, GME
 
 # although this file does make a table, for some reason, it does not create the table with the datatypes we want.
 # After you run the file and load the appropriate info about the stock tickers you want, run the sql file so the columns have appropriate datatypes.
-psql -h -d -U -f change_datatype.sql # h = host; d = database name; U = username; f = file name = change_datatype.sql
-
-
-
-# this is needed to make sure you can use the premium search api from tweepy to pull archive tweets
-pip install --upgrade git+https://github.com/tweepy/tweepy@master
+# input the appropriate parameters: h = host; d = database name; U = username; f = file name = change_datatype.sql
+psql -h -d -U -f change_datatype.sql 
 
 
 
 # next step is to pull text data. Reddit API calls are working for now.
 # This file currently pulls texts filtered to only include TSLA data after 2020-01-01
-# Also it looks through subreddits: wallstreetbets, stocks, StockMarket, Daytrading
+# Also it looks through the following subreddits: wallstreetbets, stocks, StockMarket, Daytrading
 # the keywords, dates, and subreddits to search for can be updated in the main() of this file
-python extract_reddit.py 
+# You can change the keywords or change ticker symbols based on your interests
+# After you make the necessary changes in the main() method of the .py file, run the file:
+
+python extract_reddit.py #the given code only extracts posts but you can also extract comments. You will have to process the comments differently as posts are one line in csv while comments could be multi line
 
 
+# You can also choose to extract data from Twitter. But I did not have access to their premium API so I could only access the latest 7 days worth of data.
+# If you want twitter tweets, uncomment the next line:
 
-# This file is to clean and pre-process the text data obtained from Reddit API calls
+#python extract_twitter.py
+
+
+# Next step is to run the sentiment_analysis.py file.
+# This file cleans and pre-processes the text data obtained from Reddit API calls
 # This file also creates two list of positive and negative bag of words by processing file obtained from http://mpqa.cs.pitt.edu/
-# the program outputs two dataframe: one represents raw data obtained from Reddit, the other one shows the result of sentiment analysis.
-python sentiment_analysis.py
+# you can un-comment the next line or run the command as explained in README
+#python sentiment_analysis.py
